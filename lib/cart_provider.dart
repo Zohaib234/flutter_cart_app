165e09faded89af9cabb00bf8513f3b72fe4acd2
Zohaib234 <zhbah234@gmail.com>
2023-07-15 10:03:41 +0500

    model , database and provider is set now

---
+++ lib/cart_provider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartProvider with ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice;
+
+  void _setPrefItem() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setInt('cart_item', _counter);
+    preferences.setDouble('total_price', _totalPrice);
+    notifyListeners();
+  }
+
+  void _getPrefItems() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    _counter = preferences.getInt('cart_item') ?? 0;
+    _totalPrice = preferences.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
+
+  void addTotalPrice(double productPrice) {
+    _totalPrice = _totalPrice + productPrice;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  void removeTotalPrice(double productPrice) {
+    _totalPrice = _totalPrice - productPrice;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  double getTotalPrice() {
+    _getPrefItems();
+    return _totalPrice;
+  }
+
+  void addCounter() {
+    _counter++;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  void removeCounter() {
+    _counter--;
+    _setPrefItem();
+    notifyListeners();
+  }
+
+  int getCounter() {
+    _getPrefItems();
+    return _counter;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cart_app/cart_provider.dart';
 import 'package:cart_app/product_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return ChangeNotifierProvider(create: (context)=>CartProvider(),
+    child:Builder(builder: (BuildContext context){
+      return MaterialApp(
+        title: 'CartApp',
+        theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: ProductListScreen(),
-      debugShowCheckedModeBanner: false,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: ProductListScreen(),
+        debugShowCheckedModeBanner: false,
+      );
+    }),
+
     );
   }
 }
